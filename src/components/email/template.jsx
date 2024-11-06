7cd1d73f2e2e2b06e73ef1434fa4a61663fce86a
stormyy00 <jonathan.trujillo0432@gmail.com>
2024-11-05 23:52:09 -0800

    email colors added

--- src/components/email/template.jsx
+++ src/components/email/template.jsx
@@ -12,45 +12,50 @@ import {
   Text,
   Tailwind,
 } from "@react-email/components";
-
+import LOGO from "@/public/assets/cutiehackLOGO.svg";
 const Template = ({ children, name, preview }) => {
   return (
     <Html>
       <Head />
       <Preview>{preview}</Preview>
       <Tailwind>
-        <Body className="mx-auto my-auto bg-white px-2 font-sans">
-          <Container className="mx-auto my-[40px] max-w-[465px] rounded border border-solid border-[#eaeaea] p-[20px]">
+        <Body className="mx-auto my-auto bg-white px-2 font-righteous">
+          <Container className="mx-auto my-[40px] max-w-[465px] rounded border border-solid border-[#eaeaea] bg-[#3a439b] p-[20px] text-white">
             <Section className="mt-[32px]">
               <Img
-                src="https://www.bearhack.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo.473367c8.webp&w=3840&q=75"
+                src={LOGO}
                 width="40"
                 height="37"
                 alt="Vercel"
                 className="mx-auto my-0"
               />
             </Section>
-            <Heading className="mx-0 my-[30px] p-0 text-center text-[24px] font-normal text-black">
+            <Heading className="mx-0 my-[30px] p-0 text-center text-[24px] font-normal text-white">
               Thank you for applying!
             </Heading>
-            <Text className="text-[14px] leading-[24px] text-black">
+            <Text className="text-[14px] leading-[24px]">
               Hello <strong>{name}</strong>,
             </Text>
             {children}
             <Text>
               - {data.name} Team {data.heart}
             </Text>
-            <Hr className="mx-0 my-[26px] w-full border border-solid border-[#eaeaea]" />
+            <Hr className="mx-0 my-[26px] w-full border border-solid border-[#eaeaea] text-white" />
             <Text>
-              Visit {data.domain} for more information about {data.name} and
-              follow us on {data.instagram} and {data.linkedin} for up to date
-              information and announcements.
+              Visit <span className="text-cutie-magenta">{data.domain} </span>
+              for more information about
+              <span className="text-cutie-magenta">{data.name} </span> and
+              follow us on{" "}
+              <span className="text-cutie-magenta">{data.instagram} </span> and{" "}
+              <span className="text-cutie-magenta">{data.domain} </span>for up
+              to date information and announcements.
             </Text>
-            <Text className="text-xs leading-[24px] text-[#666666]">
+
+            <Text className="text-xs leading-[24px] text-cutie-gray">
               This invitation was intended for{" "}
-              <span className="text-black">{name}</span>. If you were not
-              expecting this email, you can ignore this email. If you are
-              concerned about your account&apos;s safety, please contact{" "}
+              <span className="font-semibold text-white">{name}</span>. If you
+              were not expecting this email, you can ignore this email. If you
+              are concerned about your account&apos;s safety, please contact{" "}
               {data.email} to get in touch with us.
             </Text>
           </Container>
