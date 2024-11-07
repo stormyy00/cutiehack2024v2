b8929ff1815f8019fe1cffbc9f3279e1cb0b5c03
sunbrandon <sunabrandon@gmail.com>
2024-11-07 02:04:25 -0600

    Created tracks layout, styling not finished

--- src/components/live/tracks.tsx
+++ src/components/live/tracks.tsx
@@ -1,8 +1,27 @@
 import Title from "./title";
+import Image from "next/image";
+import { TRACKS } from "@/data/live/tracks";
+
 const Tracks = () => {
   return (
     <div className="-mt-1 flex h-screen flex-col items-center justify-center gap-10 bg-cutie-blue-300 py-10 font-righteous">
-      <Title text={"Tracks"} subtext={"Coming soon!"} />
+      <Title text="Tracks" subtext="" />
+      <div className="grid grid-cols-1 gap-8 md:grid-cols-3">
+        {TRACKS.map((track, index) => (
+          <div
+            key={index}
+            className="flex flex-col items-center text-center text-white"
+          >
+            <Image
+              src={track.image}
+              alt={track.title}
+              className="h-[200px] w-[100px]"
+            />
+            <h2 className="mt-4 text-xl font-semibold">{track.title}</h2>
+            <p className="mt-2 px-4">{track.description}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
---
+++ src/data/live/tracks.ts
@@ -0,0 +1,59 @@
+import tearblue from "@/public/assets/tearblue.svg";
+import tearpink from "@/public/assets/tearpink.svg";
+import tearpurple from "@/public/assets/tearpurple.svg";
+
+export const TRACKS = [
+  {
+    title: "1st Overall",
+    description:
+      "Awarded to the most impressive hack overall, beginners and veterans alike.",
+    image: tearpink,
+  },
+  {
+    title: "2nd Overall",
+    description:
+      "Awarded to the most impressive runner-up hack, beginners and veterans alike.",
+    image: tearpink,
+  },
+  {
+    title: "3rd Overall",
+    description:
+      "Awarded to the most impressive second runner-up hack, beginners and veterans alike.",
+    image: tearpink,
+  },
+  {
+    title: "Best Hardware",
+    description:
+      "These projects involve the best incorporation of hardware that meshes well with software.",
+    image: tearpurple,
+  },
+  {
+    title: "Best UI/UX",
+    description:
+      "These projects must have a clean, easy-to-use interface. Design is an important aspect here.",
+    image: tearpurple,
+  },
+  {
+    title: "Best Beginner",
+    description:
+      "For your hack to qualify as a Beginner Hack, Cutie Hack must be the first hackathon a majority of your team has attended.",
+    image: tearpurple,
+  },
+  {
+    title: "Most Sustainable",
+    description:
+      "These projects must focus on promoting environmental sustainability and addressing ecological challenges.",
+    image: tearblue,
+  },
+  {
+    title: "Best Social Impact",
+    description:
+      "These projects must focus on improving the world by addressing social issues and promoting positive change.",
+    image: tearblue,
+  },
+  {
+    title: "Most on Theme",
+    description: "These projects journeyed furthest beyond the stars.",
+    image: tearblue,
+  },
+];
