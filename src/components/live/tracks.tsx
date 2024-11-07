aa506c4dd16d7bc7bf9a00ecc8a02331ccac7afc
stormyy00 <jonathan.trujillo0432@gmail.com>
2024-11-07 03:04:57 -0800

    fixed respnsiveness i think

--- src/components/live/tracks.tsx
+++ src/components/live/tracks.tsx
@@ -6,22 +6,22 @@ const Tracks = () => {
   return (
     <div className="mt-[12vh] flex h-full flex-col items-center justify-center gap-0 bg-cutie-blue-300 py-10 font-righteous">
       <Title text="Tracks" subtext="" />
-      <div className="grid w-10/12 grid-cols-1 gap-0 overflow-auto md:grid-cols-3">
+      <div className="flex w-9/12 flex-wrap justify-center gap-y-10">
         {TRACKS.map((track, index) => (
           <div
             key={index}
-            className="relative flex flex-col items-center text-center text-white"
+            className="relative flex w-1/2 flex-col items-center text-center text-white md:w-1/3"
           >
             <Image
               src={track.image}
               alt={track.title}
-              className={`w-5/12 md:w-full ${track.className}`}
+              className={`w-full scale-125 md:scale-100 ${track.className}`}
             />
             <div className={`absolute text-center ${track.text}`}>
-              <h2 className="mt-4 w-full text-base font-semibold md:w-3/5 md:text-2xl">
+              <h2 className="mt-4 w-full text-xs font-semibold md:w-3/5 md:text-2xl">
                 {track.title}
               </h2>
-              <p className="mt-2 w-full px-4 text-xs md:w-3/5 md:text-sm">
+              <p className="mt-2 w-full px-0 text-[7px] md:w-3/5 md:text-sm">
                 {track.description}
               </p>
             </div>
--- src/data/live/tracks.ts
+++ src/data/live/tracks.ts
@@ -8,8 +8,8 @@ export const TRACKS = [
     description:
       "Awarded to the most impressive hack overall, beginners and veterans alike.",
     image: tearpink,
-    className: "-rotate-12  top-0 left-20",
-    text: "top-[40%] left-[15%]",
+    className: "-rotate-12  top-0",
+    text: " top-[30%] left-[9%]  md:top-[35%] md:left-[17%] -rotate-[20deg] w-6/12 md:w-8/12 ",
   },
   {
     title: "2nd Overall",
@@ -17,7 +17,7 @@ export const TRACKS = [
       "Awarded to the most impressive runner-up hack, beginners and veterans alike.",
     image: tearpink,
     className: "rotate-12 md:absolute top-[22%]",
-    text: "top-[57%] left-[13%]",
+    text: " top-[25%] left-[8%]  md:top-[57%] md:left-[16%] w-7/12 md:w-9/12",
   },
   {
     title: "3rd Overall",
@@ -25,7 +25,7 @@ export const TRACKS = [
       "Awarded to the most impressive second runner-up hack, beginners and veterans alike.",
     image: tearpink,
     className: "rotate-[24deg] md:absolute top-0 ",
-    text: "top-[33%] left-[13%] ",
+    text: " top-[20%] left-[12%]  md:top-[33%] md:left-[19%] w-6/12 md:w-8/12 ",
   },
   {
     title: "Best Hardware",
@@ -33,7 +33,7 @@ export const TRACKS = [
       "These projects involve the best incorporation of hardware that meshes well with software.",
     image: tearpurple,
     className: "-rotate-6",
-    text: "top-[40%] left-[15%]",
+    text: " top-[30%] left-[8%]  md:top-[40%] md:left-[15%] w-7/12 md:w-11/12",
   },
   {
     title: "Best UI/UX",
@@ -41,7 +41,7 @@ export const TRACKS = [
       "These projects must have a clean, easy-to-use interface. Design is an important aspect here.",
     image: tearpurple,
     className: "-rotate-[24deg] md:absolute top-[15%]",
-    text: "top-[55%] left-[17%]",
+    text: "  top-[33%] left-[12%]  md:top-[51%] md:left-[17%] w-7/12 md:w-9/12",
   },
   {
     title: "Best Beginner",
@@ -49,7 +49,7 @@ export const TRACKS = [
       "For your hack to qualify as a Beginner Hack, Cutie Hack must be the first hackathon a majority of your team has attended.",
     image: tearpurple,
     className: "rotate-0",
-    text: "top-[36%] left-[17%]",
+    text: " top-[26%] left-[12%]  md:top-[36%] md:left-[18%] w-7/12 md:w-9/12",
   },
   {
     title: "Most Sustainable",
@@ -57,7 +57,7 @@ export const TRACKS = [
       "These projects must focus on promoting environmental sustainability and addressing ecological challenges.",
     image: tearblue,
     className: "-rotate-12",
-    text: "top-[30%] left-[15%] w-3/4 -rotate-[20deg]",
+    text: " top-[20%] left-[4%]  md:top-[26%] md:left-[14%] w-3/4 -rotate-[20deg] w-7/12 md:w-9/12",
   },
   {
     title: "Best Social Impact",
@@ -65,13 +65,13 @@ export const TRACKS = [
       "These projects must focus on improving the world by addressing social issues and promoting positive change.",
     image: tearblue,
     className: "rotate-12 md:absolute top-[15%]",
-    text: "top-[46%] left-[16%] w-3/4",
+    text: " top-[20%] left-[8%]  md:top-[46%] md:left-[15.5%] w-7/12 md:w-9/12",
   },
   {
     title: "Most on Theme",
     description: "These projects journeyed furthest beyond the stars.",
     image: tearblue,
     className: "rotate-[24deg]",
-    text: "top-[40%] left-[15%] w-4/5",
+    text: " top-[20%] left-[11%]  md:top-[30%] md:left-[16%] w-6/12 md:w-9/12",
   },
 ];
