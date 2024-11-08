1d3432a0cf1a7f22483a8f4b35da2887c7edbed8
stormyy00 <jonathan.trujillo0432@gmail.com>
2024-11-08 12:22:12 -0800

    fix faqqqqqqqqqqqqqq

--- src/components/live/faq.tsx
+++ src/components/live/faq.tsx
@@ -14,7 +14,7 @@ const FAQ = () => {
       id="faq"
       className="w-ffull flex flex-col items-center bg-cutie-blue-300 py-10 font-righteous text-white"
     >
-      <div className="z-30 flex items-center justify-center gap-2 py-4 align-middle text-7xl">
+      {/* <div className="z-30 flex items-center justify-center gap-2 py-4 align-middle text-7xl">
         <h1>F</h1>
         <Image
           src={UFO}
@@ -22,7 +22,7 @@ const FAQ = () => {
           className="-mt-5 size-1/3 animate-jiggle-12s"
         />
         <h1>Q</h1>
-      </div>
+      </div> */}
       <div className="w-11/12 rounded-3xl border-8 border-[#A22282] bg-[#61114D] p-6 shadow-lg md:w-5/12">
         <Accordion type="single" collapsible className="w-full">
           {QUESTIONS.map(({ question, answer }, index) => (
