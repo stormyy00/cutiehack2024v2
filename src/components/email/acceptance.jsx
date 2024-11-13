cf33b83a1b8f9a5f3bbaff4621f2acf487ddc968
stormyy00 <jonathan.trujillo0432@gmail.com>
2024-11-13 01:30:26 -0800

    added wolfram link

--- src/components/email/acceptance.jsx
+++ src/components/email/acceptance.jsx
@@ -1,11 +1,11 @@
 import data from "@/data/config";
 import Template from "./template";
-import { Button, Section, Text } from "@react-email/components";
+import { Button, Section, Text, Link } from "@react-email/components";
 
 const Acceptance = ({ name, position, preview }) => {
   return (
     <Template name={name} preview={preview}>
-      <Text>
+      <Text className="text-white">
         🎉 Congratulations 🎉 You have been accepted into {data.name} as a{" "}
         <strong>{position}</strong>
       </Text>
@@ -21,6 +21,28 @@ const Acceptance = ({ name, position, preview }) => {
           Join Discord
         </Button>
       </Section>
+      <Text className="text-white">
+        We'd also like to thank Wolgram for sponoring us by giving us
+        complimentary access to our development platform and the WolframAlpha
+        API for six months, just click
+        <Link
+          href={"https://account.wolfram.com/redeem/qzRiversideCutieHack1124"}
+          className="text-[#FF00C0]"
+        >
+          {" "}
+          here{" "}
+        </Link>
+      </Text>
+      <Text className="text-xs leading-[24px] text-[#DADADA]">
+        This link will take you to a sign-in page and prompt you to log in using
+        your Wolfram ID. If you don't already have a Wolfram ID, you'll be
+        prompted to create one using an active email address. Once logged in,
+        you will see in the Downloads section which platforms of the software
+        you have access to, with their accompanying activation keys. Please
+        download the desktop version to your machine, boot up the application
+        and use the associated activation key when prompted. This will grant you
+        full access for six months from your activation date.
+      </Text>
 
       <Text>We look forward to seeing you there!</Text>
     </Template>
