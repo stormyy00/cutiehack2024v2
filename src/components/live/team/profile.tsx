1d4f84364a25a54648d9e6267e2695d539782ee1
Taah <admin@taah.dev>
2024-11-14 02:41:26 -0800

    format..

--- src/components/live/team/profile.tsx
+++ src/components/live/team/profile.tsx
@@ -14,7 +14,8 @@ const Profile = ({
     <div className="flex w-fit flex-col items-center justify-center text-center font-righteous text-white">
       <div className="relative mb-6 flex w-10/12 items-center justify-center">
         <Image unoptimized src={Border} alt="window" />
-        <Image unoptimized
+        <Image
+          unoptimized
           src={image}
           alt={`${name}'s Image`}
           className="absolute inset-0 left-[17%] top-[16%] h-[68%] w-[68%] rounded-full object-cover"
