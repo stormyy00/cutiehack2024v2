c7cc6b2366521f2c0b42c716f7b5d639e67be685
stormyy00 <jonathan.trujillo0432@gmail.com>
2024-11-14 23:38:46 -0800

    added picture to judges

--- src/data/live/judges.ts
+++ src/data/live/judges.ts
@@ -1,47 +1,88 @@
+import Alica from "@/public/judges/Alice_Thai.png";
+import Bobby from "@/public/judges/Bobby_Lerias.png";
+import Cameron from "@/public/judges/Cameron_Tsai.png";
+import Ethan from "@/public/judges/Ethan_Roos.png";
+import Jerry from "@/public/judges/Jerry_LI.png";
+import Joshua from "@/public/judges/Joshua_Candelaria.png";
+import Karan from "@/public/judges/Karan_Bhogal.png";
+import Mariam from "@/public/judges/Mariam_Golwalla.png";
+import Paulian from "@/public/judges/Paulian_Le.png";
+import Rachit from "@/public/judges/Rachit_Prajapati.png";
+import Raidah from "@/public/judges/Raidah_Fairooz.png";
+import Samarth from "@/public/judges/Samarth_Srinivasa.png";
+import Sathya from "@/public/judges/Sathya_Srinivasan.png";
+import Yogitha from "@/public/judges/Yogitha_Shastri.png";
+
 export const JUDGES = [
   {
     name: "Alicia Thai ",
-    image: "",
+    image: Alica,
     title: "Software Engineer @ Google",
   },
   {
+    name: "Bobby Lerias",
+    image: Bobby,
+    title: "Product Designer",
+  },
+  {
     name: "Cameron Tsai",
-    image: "",
+    image: Cameron,
     title: "Recent College Graduate",
   },
   {
+    name: "Ethan Roos",
+    image: Ethan,
+    title: "CS Grad Student",
+  },
+  {
     name: "Jerry Li",
-    image: "",
+    image: Jerry,
     title: "Vice President of ACM",
   },
   {
     name: "Joshua Candelaria",
-    image: "",
+    image: Joshua,
     title: "Graduate Student",
   },
   {
     name: "Karan Bhogal",
-    image: "",
+    image: Karan,
     title: "SWE @ Cisco",
   },
   {
     name: "Paulian Le",
-    image: "",
+    image: Paulian,
     title: "Previous Operations Lead",
   },
   {
-    name: "Sathya Srinivasan",
-    image: "",
-    title: "Software Engineer @ Google",
+    name: "Mariam Golwalla",
+    image: Mariam,
+    title: "Cybersecurity Analyst",
+  },
+
+  {
+    name: "Raidah Fairooz",
+    image: Raidah,
+    title: "BCOE Alumni",
+  },
+  {
+    name: "Rachit Prajapati",
+    image: Rachit,
+    title: "Graduate student",
   },
   {
     name: "Samarth Srinivasa",
-    image: "",
+    image: Samarth,
+    title: "Hackathon Lead",
+  },
+  {
+    name: "Sathya Srinivasan",
+    image: Sathya,
     title: "Hackathon Lead",
   },
   {
     name: "Yogitha Shastri",
-    image: "",
-    title: "Former BearHack Director, MSMDEng",
+    image: Yogitha,
+    title: "Former BearHack Director",
   },
 ];
