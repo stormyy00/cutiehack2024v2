11c70d23cbdf1a1cc6ff88552941c78e84656b29
stormyy00 <jonathan.trujillo0432@gmail.com>
2024-11-14 23:48:48 -0800

    z index

--- src/components/live/judges.tsx
+++ src/components/live/judges.tsx
@@ -5,7 +5,7 @@ const Judges = () => {
   return (
     <div className="-mt-1 flex flex-col items-center justify-center gap-10 bg-cutie-blue-300 py-10 font-righteous">
       <Title text={"Judges"} subtext={"Thank you so much!"} />
-      <div className="mt-[8vh] flex w-full flex-wrap justify-center gap-8 py-2 md:w-10/12 md:gap-6">
+      <div className="z-20 mt-[8vh] flex w-full flex-wrap justify-center gap-8 py-2 md:w-10/12 md:gap-6">
         {JUDGES.map((judge, index) => (
           <div key={index} className="w-1/3 md:w-fit">
             <Profile name={judge.name} image={judge.image} team={judge.title} />
